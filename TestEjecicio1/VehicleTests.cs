a0112de7ff5deeb657e66c7fe21b6b525a8e38b3
joaquin03 <janduano@gmail.com>
2023-09-26 13:59:19 -0300

    fin de template ejecrico

--- TestEjecicio1/VehicleTests.cs
+++ TestEjecicio1/VehicleTests.cs
@@ -11,37 +11,42 @@ public class VehicleTests
     [TestInitialize]
     public void TestInitialize()
     {
-        
+        vehicleDealership = new VehicleDealership();
     }
     
     [TestMethod]
     public void TestIsClassicCarTrue()
     {
-       
+        Car car = new Car { Year = 1990, Brand = new Brand { ClassicBrands = true } };
+        Assert.IsTrue(vehicleDealership.IsClassicVehicle(car));
     }
 
     [TestMethod]
     public void TestIsClassicCarFalseDueToYear()
     {
-        
+        Car car = new Car { Year = 2005, Brand = new Brand { ClassicBrands = true } };
+        Assert.IsFalse(vehicleDealership.IsClassicVehicle(car));
     }
 
     [TestMethod]
     public void TestIsClassicCarFalseDueToBrand()
     {
-        
+        Car car = new Car { Year = 1990, Brand = new Brand { ClassicBrands = false } };
+        Assert.IsFalse(vehicleDealership.IsClassicVehicle(car));
     }
 
     [TestMethod]
     public void TestIsClassicTruckTrue()
     {
-       
+        Truck truck = new Truck { Year = 1980 };
+        Assert.IsTrue(vehicleDealership.IsClassicVehicle(truck));
     }
 
     [TestMethod]
     public void TestIsClassicTruckFalse()
     {
-        
+        Truck truck = new Truck { Year = 2000 };
+        Assert.IsFalse(vehicleDealership.IsClassicVehicle(truck));
     }
     
     
